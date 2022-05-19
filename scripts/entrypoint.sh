7ea65c2eccca2fede4e6edbcc6dd88a21458218e
Sei-Yukinari <sei.yukinari@nextremer.com>
2022-05-19 12:48:48 +0900

    fix entrypoint shell

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,14 +2,12 @@
 
 set -e
 
-# create Database for testing
+# create database for testing
 echo 'start setup test db.'
 apt-get -y install default-mysql-client
 mysql -h "${DB_HOST}" -P "${DB_PORT}" -u "${DB_USER}" -p"${DB_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS "$TEST_DB_NAME";"
-goose -env test up
-echo "created & migrated test db"
 
-# RDB migration
+# database migration
 goose up
 goose -env test up
 echo "migrated."
